60c5d4013ee9e8ddb63a986488ccebcd04d2e6ed
jordi <jordi.marsal@gmail.com>
2021-11-19 16:20:18 +0100

    feat: populator for katas-java mongo db

---
+++ src/main/java/net/jordimp/katas/config/MongoConfig.java
@@ -0,0 +1,19 @@
+package net.jordimp.katas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() {
+        final Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[] { new ClassPathResource("data.json") });
+        return factory;
+    }
+
+}
